5d36faa4006eb95f2a3bce39ed02940c10d11bab
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-26 10:54:45 -0800

    Home component toggleUnitsModal

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import UnitsModal from '../components/units-modal';
 
-export default function Home(props) {
-  return (
-    <>
-      <header>
-        <div className="overlay d-flex flex-column align-items-center justify-content-around">
-          <img src="./bblogo.png" alt="Ballistics Buddy logo" />
-          <div className="text-center">
-            <p>Select Units:</p>
-            <Button variant="light" className="mr-3">MOA</Button>
-            <Button variant="light">MRAD</Button>
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showUnitsModal: false
+    };
+    this.toggleUnitsModal = this.toggleUnitsModal.bind(this);
+  }
+
+  toggleUnitsModal() {
+    this.setState({
+      showUnitsModal: !this.state.showUnitsModal
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <div className="overlay d-flex flex-column align-items-center justify-content-around">
+            <img src="./bblogo.png" alt="Ballistics Buddy logo" />
+            <div className="text-center">
+              <p>Select Units:</p>
+              <Button variant="light" className="mr-3">MOA</Button>
+              <Button variant="light">MRAD</Button>
+              <div>
+                <Button variant="light" className="mt-3" onClick={this.toggleUnitsModal}>?</Button>
+              </div>
+            </div>
             <div>
-              <Button variant="light" className="mt-3">?</Button>
+              <p className="m-0 text-center">Remember to always use</p>
+              <p className="safe-shooting m-0 text-center">Safe Shooting Practices</p>
             </div>
           </div>
-          <div>
-            <p className="m-0 text-center">Remember to always use</p>
-            <p className="safe-shooting m-0 text-center">Safe Shooting Practices</p>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+        </header>
+        <UnitsModal show={this.state.showUnitsModal} toggle={this.toggleUnitsModal} />
+      </>
+    );
+  }
 }
