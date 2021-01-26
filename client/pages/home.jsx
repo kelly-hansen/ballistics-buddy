f01b60a2c0dd59d772c6c85fc973006d80c0c70a
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-26 10:12:10 -0800

    Safe Shooting Practices

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -12,8 +12,12 @@ export default function Home(props) {
             <Button variant="light" className="mr-3">MOA</Button>
             <Button variant="light">MRAD</Button>
             <div>
-              <Button variant="dark" className="mt-3">?</Button>
+              <Button variant="light" className="mt-3">?</Button>
             </div>
+          </div>
+          <div>
+            <p className="m-0 text-center">Remember to always use</p>
+            <p className="safe-shooting m-0 text-center">Safe Shooting Practices</p>
           </div>
         </div>
       </header>
