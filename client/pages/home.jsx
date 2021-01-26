3225216f2c91643c46e83ad2a62daa75db82adf9
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-26 09:47:05 -0800

    New logo/header layout with overlay

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
+      <header>
+        <div className="overlay d-flex flex-column justify-content-center">
+          <img src="./bblogo.png" alt="Ballistics Buddy logo" />
+        </div>
+      </header>
     </>
   );
 }
