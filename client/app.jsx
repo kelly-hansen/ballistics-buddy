51173cc7c82c4046493bb6488314b66ba1639c88
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-26 11:14:45 -0800

    Initialize app state.units

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Home from './pages/home';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      units: null
+    };
+  }
+
   render() {
     return <Home />;
   }
