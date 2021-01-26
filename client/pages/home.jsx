59262a953207af494a902506660d2cb0059231d1
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-26 10:03:46 -0800

    Select Units buttons

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default function Home(props) {
   return (
     <>
       <header>
-        <div className="overlay d-flex flex-column justify-content-center">
+        <div className="overlay d-flex flex-column align-items-center justify-content-around">
           <img src="./bblogo.png" alt="Ballistics Buddy logo" />
+          <div className="text-center">
+            <p>Select Units:</p>
+            <Button variant="light" className="mr-3">MOA</Button>
+            <Button variant="light">MRAD</Button>
+            <div>
+              <Button variant="dark" className="mt-3">?</Button>
+            </div>
+          </div>
         </div>
       </header>
     </>
