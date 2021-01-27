a40adc2229a18e690ee9bf1ce878962127365e4d
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-27 13:11:24 -0800

    Buttons styling and focus

--- client/components/data-tabs.jsx
+++ client/components/data-tabs.jsx
@@ -2,15 +2,39 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default class DataTabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'CALCULATE'
+    };
+    this.changeTabs = this.changeTabs.bind(this);
+  }
+
+  changeTabs(e) {
+    this.setState({
+      view: e.target.textContent
+    });
+  }
+
   render() {
     return (
       <Container className="data-tabs-cont pt-5" id="calculate-data">
         <Row>
-          <Col className="d-flex justify-content-center">
-            <button className="tab-button">CALCULATE</button>
+          <Col className="d-flex justify-content-end">
+            <button
+              className={`tab-button${this.state.view === 'CALCULATE' ? ' selected' : ''}`}
+              onClick={this.changeTabs}
+            >
+              CALCULATE
+            </button>
           </Col>
-          <Col className="d-flex justify-content-center">
-            <button className="tab-button">REFERENCE DATA</button>
+          <Col className="d-flex justify-content-start">
+            <button
+              className={`tab-button${this.state.view === 'REFERENCE DATA' ? ' selected' : ''}`}
+              onClick={this.changeTabs}
+            >
+              REFERENCE DATA
+            </button>
           </Col>
         </Row>
       </Container>
