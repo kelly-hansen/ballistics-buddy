535e612c45f7f599067b3caf947b389f2867850a
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-30 10:07:41 -0800

    Render ballistics table with dummy data

--- client/components/ballistics-table.jsx
+++ client/components/ballistics-table.jsx
@@ -5,12 +5,24 @@ export default class BallisticsTable extends React.Component {
   render() {
     const tableData = this.props.caliber1Data.ballisticsData.map((dataItem, i) => {
       const bulletDrop = dataItem.distance === 0 ? '-' : dataItem.bulletDrop;
+      let adjustment;
+      if (dataItem.distance === 0) {
+        adjustment = '-';
+      } else {
+        const distanceMultiplier = dataItem.distance / 100;
+        if (this.context.units === 'MOA') {
+          adjustment = dataItem.bulletDrop / distanceMultiplier / 1.047;
+        } else if (this.context.units === 'MRAD') {
+          adjustment = dataItem.bulletDrop / distanceMultiplier / 3.6;
+        }
+        adjustment = adjustment.toFixed(2);
+      }
 
       return (
         <tr key={`dataItem${i}`}>
           <td>{dataItem.distance}</td>
           <td>{bulletDrop}</td>
-          <td></td>
+          <td>{adjustment}</td>
         </tr>
       );
     });
@@ -21,7 +33,7 @@ export default class BallisticsTable extends React.Component {
           <tr>
             <th>Distance (yds)</th>
             <th>Bullet Drop (in)</th>
-            <th>Adj</th>
+            <th>{this.context.units}</th>
           </tr>
         </thead>
         <tbody>
