e47d7641e86fcc7212ed7caf266975d53a3b22ca
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-30 10:42:45 -0800

    Schema sql

---
+++ database/schema.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS "public";
+
+CREATE TABLE "calibers" (
+        "caliberId" serial NOT NULL,
+        "calibers" TEXT NOT NULL,
+        CONSTRAINT "calibers_pk" PRIMARY KEY ("caliberId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "ballisticsData" (
+        "dataPointId" serial NOT NULL,
+        "caliberId" int NOT NULL,
+        "distance" int NOT NULL,
+        "bulletDrop" decimal NOT NULL,
+        CONSTRAINT "ballisticsData_pk" PRIMARY KEY ("dataPointId")
+) WITH (
+  OIDS=FALSE
+);
