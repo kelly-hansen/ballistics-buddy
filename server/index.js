5c774fbfe799fe639bb1e89e4d144f5191ec45dc
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-30 17:47:04 -0800

    server get route with error handling

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
 
 const app = express();
 
 app.use(staticMiddleware);
+
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.get('/api/ballistics-data', (req, res, next) => {
+  const caliber = req.headers.caliber;
+  if (!caliber) {
+    throw new ClientError(400, 'caliber is a required field');
+  }
+  const sql = `
+    select "distance", "bulletDrop"
+    from "ballisticsData"
+    join "calibers" using ("caliberId")
+    where "caliberId" = $1;
+  `;
+  const params = [caliber];
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
