277ef3be01ae70a74bd052f372dc654842ca6027
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-31 10:32:20 -0800

    fetching and rendering caliber data

--- client/components/charts.jsx
+++ client/components/charts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Form } from 'react-bootstrap';
+import { Row, Col, Form, Spinner } from 'react-bootstrap';
 import BallisticsChart from './ballistics-chart';
 import BallisticsTable from './ballistics-table';
 
@@ -7,19 +7,21 @@ export default class Charts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      caliber: '.223 Remington'
+      status: null,
+      caliber1: '.223 Remington',
+      caliber1Data: null
     };
     this.handleChangeCaliber = this.handleChangeCaliber.bind(this);
   }
 
   handleChangeCaliber(e) {
+    this.getBallisticsData(e.target.value, 'caliber1');
     this.setState({
-      caliber: e.target.value
+      caliber1: e.target.value
     });
-    this.getBallisticsData(e.target.value);
   }
 
-  getBallisticsData(caliber) {
+  getBallisticsData(caliber, designation) {
     fetch('/api/ballistics-data', {
       headers: {
         'Content-Type': 'application/json',
@@ -28,11 +30,34 @@ export default class Charts extends React.Component {
     })
       .then(res => res.json())
       .then(result => {
-        console.log(result);
+        const ballisticsData = result.map(dataItem => {
+          return {
+            distance: dataItem.distance,
+            bulletDrop: parseFloat(dataItem.bulletDrop, 10)
+          };
+        });
+        const caliberData = {
+          caliber,
+          ballisticsData
+        };
+        if (designation === 'caliber1') {
+          this.setState({
+            caliber1Data: caliberData,
+            status: 'loaded'
+          });
+        } else if (designation === 'caliber2') {
+          this.setState({
+            caliber2Data: caliberData
+          });
+        }
       })
       .catch(err => {
         console.error(err);
       });
+  }
+
+  componentDidMount() {
+    this.getBallisticsData('.223 Remington', 'caliber1');
   }
 
   render() {
@@ -136,40 +161,53 @@ export default class Charts extends React.Component {
       ]
     };
 
-    return (
-      <>
-        <Row className="mt-5 d-flex justify-content-center">
-          <Col md={8} lg={6} className="d-flex justify-content-center">
-            <Form>
-              <Form.Group>
-                <Form.Label>Caliber</Form.Label>
-                <Form.Control as="select" className="dark" value={this.state.caliber} onChange={this.handleChangeCaliber}>
-                  <option value=".223 Remington">.223 Remington</option>
-                  <option value=".300 Win Mag">.300 Win Mag</option>
-                  <option value=".308 Winchester">.308 Winchester</option>
-                  <option value="6.5mm Creedmoor">6.5mm Creedmoor</option>
-                  <option value=".50 BMG">.50 BMG</option>
-                </Form.Control>
-              </Form.Group>
-            </Form>
+    let content;
+    if (!this.state.status) {
+      content = (
+        <Row className="mt-5">
+          <Col className="mt-5 d-flex justify-content-center">
+            <Spinner animation="grow" variant="light" />
           </Col>
         </Row>
-        <Row className="mt-5 d-flex justify-content-center">
-          <Col md={9} lg={6} className="mb-5">
-            <BallisticsChart caliber1Data={dummyData} caliber2Data={dummyData2} />
-          </Col>
-          <Col md={9} lg={6}>
-            <BallisticsTable caliber1Data={dummyData} />
-          </Col>
-        </Row>
-        <Row className="mt-5 d-flex justify-content-center">
-          <Col md={8} lg={6}>
-            <p className="text-center">
-              Note: These are baseline figures for reference using a 100-yard zero. Actual performance will vary based on type of ammunition, rifle, and weather conditions.
-            </p>
-          </Col>
-        </Row>
-      </>
-    );
+      );
+    } else if (this.state.status === 'loaded') {
+      content = (
+        <>
+          <Row className="mt-5 d-flex justify-content-center">
+            <Col md={8} lg={6} className="d-flex justify-content-center">
+              <Form>
+                <Form.Group>
+                  <Form.Label>Caliber</Form.Label>
+                  <Form.Control as="select" className="dark" value={this.state.caliber1} onChange={this.handleChangeCaliber}>
+                    <option value=".223 Remington">.223 Remington</option>
+                    <option value=".300 Win Mag">.300 Win Mag</option>
+                    <option value=".308 Winchester">.308 Winchester</option>
+                    <option value="6.5mm Creedmoor">6.5mm Creedmoor</option>
+                    <option value=".50 BMG">.50 BMG</option>
+                  </Form.Control>
+                </Form.Group>
+              </Form>
+            </Col>
+          </Row>
+          <Row className="mt-5 d-flex justify-content-center">
+            <Col md={9} lg={6} className="mb-5">
+              <BallisticsChart caliber1Data={this.state.caliber1Data} caliber2Data={dummyData2} />
+            </Col>
+            <Col md={9} lg={6}>
+              <BallisticsTable caliber1Data={this.state.caliber1Data} />
+            </Col>
+          </Row>
+          <Row className="mt-5 d-flex justify-content-center">
+            <Col md={8} lg={6}>
+              <p className="text-center">
+                Note: These are baseline figures for reference using a 100-yard zero. Actual performance will vary based on type of ammunition, rifle, and weather conditions.
+              </p>
+            </Col>
+          </Row>
+        </>
+      );
+    }
+
+    return content;
   }
 }
