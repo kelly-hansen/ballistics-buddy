f38cab8b5987e8521e24c774d410bf4bb97f9bbd
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-04 15:31:07 -0800

    Convert DataTabs to function component

--- client/components/data-tabs.jsx
+++ client/components/data-tabs.jsx
@@ -1,53 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Charts from './charts';
 import Calculate from './calculate';
 
-export default class DataTabs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      view: 'CHARTS'
-    };
-    this.changeTabs = this.changeTabs.bind(this);
-    this.tabsRef = React.createRef();
-  }
+export default function DataTabs() {
+  const [view, setView] = useState('CHARTS');
 
-  changeTabs(e) {
+  const tabsRef = React.createRef();
+
+  function changeTabs(e) {
     e.target.blur();
-    this.setState({
-      view: e.target.textContent
-    });
+    setView(e.target.textContent);
   }
 
-  componentDidMount() {
-    window.scrollTo(0, this.tabsRef.current.getBoundingClientRect().y);
-  }
+  useEffect(() => {
+    window.scrollTo(0, tabsRef.current.getBoundingClientRect().y);
+  }, []);
 
-  render() {
-    return (
-      <Container className="data-tabs-cont pt-5" ref={this.tabsRef}>
-        <Row>
-          <Col className="d-flex justify-content-end">
-            <button
-              className={`general-button tab-button${this.state.view === 'CHARTS' ? ' selected' : ''}`}
-              onClick={this.changeTabs}
-            >
-              CHARTS
-            </button>
-          </Col>
-          <Col className="d-flex justify-content-start">
-            <button
-              className={`general-button tab-button${this.state.view === 'CALCULATE' ? ' selected' : ''}`}
-              onClick={this.changeTabs}
-            >
-              CALCULATE
-            </button>
-          </Col>
-        </Row>
-        {this.state.view === 'CHARTS' && <Charts />}
-        {this.state.view === 'CALCULATE' && <Calculate />}
-      </Container>
-    );
-  }
+  return (
+    <Container className="data-tabs-cont pt-5" ref={tabsRef}>
+      <Row>
+        <Col className="d-flex justify-content-end">
+          <button
+            className={`general-button tab-button${view === 'CHARTS' ? ' selected' : ''}`}
+            onClick={changeTabs}
+          >
+            CHARTS
+          </button>
+        </Col>
+        <Col className="d-flex justify-content-start">
+          <button
+            className={`general-button tab-button${view === 'CALCULATE' ? ' selected' : ''}`}
+            onClick={changeTabs}
+          >
+            CALCULATE
+          </button>
+        </Col>
+      </Row>
+      {view === 'CHARTS' && <Charts />}
+      {view === 'CALCULATE' && <Calculate />}
+    </Container>
+  );
 }
