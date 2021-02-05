3cc1085e701583bc5d2d95521ca2402040151acd
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-05 11:39:10 -0800

    Convert Calculate to function component

--- client/components/calculate.jsx
+++ client/components/calculate.jsx
@@ -1,148 +1,122 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Row, Col, Form } from 'react-bootstrap';
 import AppContext from '../lib/app-context';
 
-export default class Calculate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      distance: '',
-      adjustment: '',
-      bulletDrop: '',
-      status: '',
-      calculated: null
-    };
-    this.handleChangeDistance = this.handleChangeDistance.bind(this);
-    this.handleChangeAdjustment = this.handleChangeAdjustment.bind(this);
-    this.handleChangeBulletDrop = this.handleChangeBulletDrop.bind(this);
-    this.calculateAdjustment = this.calculateAdjustment.bind(this);
-    this.calculateBulletDrop = this.calculateBulletDrop.bind(this);
-    this.formSubmit = this.formSubmit.bind(this);
-    this.inputRef = React.createRef();
+export default function Calculate() {
+  const [distance, setDistance] = useState('');
+  const [adjustment, setAdjustment] = useState('');
+  const [bulletDrop, setBulletDrop] = useState('');
+  const [status, setStatus] = useState('');
+  const [calculated, setCalculated] = useState(null);
+
+  function handleChangeDistance(e) {
+    setDistance(e.target.value);
+    setCalculated(null);
   }
 
-  handleChangeDistance(e) {
-    this.setState({
-      distance: e.target.value,
-      calculated: null
-    });
+  function handleChangeAdjustment(e) {
+    setAdjustment(e.target.value);
+    setCalculated(null);
   }
 
-  handleChangeAdjustment(e) {
-    this.setState({
-      adjustment: e.target.value,
-      calculated: null
-    });
+  function handleChangeBulletDrop(e) {
+    setBulletDrop(e.target.value);
+    setCalculated(null);
   }
 
-  handleChangeBulletDrop(e) {
-    this.setState({
-      bulletDrop: e.target.value,
-      calculated: null
-    });
-  }
+  const { units } = useContext(AppContext);
 
-  calculateAdjustment() {
-    const distance = parseFloat(this.state.distance, 10);
-    const bulletDrop = parseFloat(this.state.bulletDrop, 10);
-    if (isNaN(distance) || isNaN(bulletDrop)) {
-      this.setState({
-        status: 'Please enter a number in the Distance and Bullet Drop fields.'
-      });
+  function calculateAdjustment() {
+    const distanceNum = parseFloat(distance, 10);
+    const bulletDropNum = parseFloat(bulletDrop, 10);
+    if (isNaN(distanceNum) || isNaN(bulletDropNum)) {
+      setStatus('Please enter a number in the Distance and Bullet Drop fields.');
       return;
     }
-    let adjustment;
-    const distanceMultiplier = distance / 100;
-    if (this.context.units === 'MOA') {
-      adjustment = bulletDrop / distanceMultiplier / 1.047;
-    } else if (this.context.units === 'MRAD') {
-      adjustment = bulletDrop / distanceMultiplier / 3.6;
+    let adjustmentNum;
+    const distanceMultiplier = distanceNum / 100;
+    if (units === 'MOA') {
+      adjustmentNum = bulletDropNum / distanceMultiplier / 1.047;
+    } else if (units === 'MRAD') {
+      adjustmentNum = bulletDropNum / distanceMultiplier / 3.6;
     }
-    adjustment = adjustment.toFixed(2);
-    this.setState({
-      adjustment,
-      status: '',
-      calculated: 'Adjustment'
-    });
+    adjustmentNum = adjustmentNum.toFixed(2);
+    setAdjustment(adjustmentNum);
+    setStatus('');
+    setCalculated('Adjustment');
   }
 
-  calculateBulletDrop() {
-    const distance = parseFloat(this.state.distance, 10);
-    const adjustment = parseFloat(this.state.adjustment, 10);
-    if (isNaN(distance) || isNaN(adjustment)) {
-      this.setState({
-        status: `Please enter a number in the Distance and ${this.context.units} fields.`
-      });
+  function calculateBulletDrop() {
+    const distanceNum = parseFloat(distance, 10);
+    const adjustmentNum = parseFloat(adjustment, 10);
+    if (isNaN(distanceNum) || isNaN(adjustmentNum)) {
+      setStatus(`Please enter a number in the Distance and ${this.context.units} fields.`);
       return;
     }
-    let bulletDrop;
-    const distanceMultiplier = distance / 100;
-    if (this.context.units === 'MOA') {
-      bulletDrop = adjustment * distanceMultiplier * 1.047;
-    } else if (this.context.units === 'MRAD') {
-      bulletDrop = adjustment * distanceMultiplier * 3.6;
+    let bulletDropNum;
+    const distanceMultiplier = distanceNum / 100;
+    if (units === 'MOA') {
+      bulletDropNum = adjustmentNum * distanceMultiplier * 1.047;
+    } else if (units === 'MRAD') {
+      bulletDropNum = adjustmentNum * distanceMultiplier * 3.6;
     }
-    bulletDrop = bulletDrop.toFixed(2);
-    this.setState({
-      bulletDrop,
-      status: '',
-      calculated: 'Bullet Drop'
-    });
+    bulletDropNum = bulletDropNum.toFixed(2);
+    setBulletDrop(bulletDropNum);
+    setStatus('');
+    setCalculated('Bullet Drop');
   }
 
-  formSubmit(e) {
+  function formSubmit(e) {
     e.preventDefault();
   }
 
-  componentDidMount() {
-    this.inputRef.current.focus();
-  }
+  const inputRef = React.createRef();
 
-  render() {
-    return (
-      <>
-        <Row className="mt-5 d-flex justify-content-center">
-          <Col md={8} lg={6}>
-            <p className="text-center">
-              {`Enter Distance and ${this.context.units} scope adjustment to calculate Bullet Drop, or enter Distance and Bullet Drop to calculate ${this.context.units} scope adjustment.`}
-            </p>
-          </Col>
-        </Row>
-        <Row className="d-flex justify-content-center my-5">
-          <Col md={6} className="d-flex justify-content-center">
-            <Form onSubmit={this.formSubmit}>
-              <Form.Group controlId="distance">
-                <Form.Label>Distance (yds)</Form.Label>
-                <div className="d-flex">
-                  <Form.Control className="w-50 mr-2 dark" ref={this.inputRef} value={this.state.distance} onChange={this.handleChangeDistance} />
-                  <div className="w-50 ml-2"></div>
-                </div>
-              </Form.Group>
-              <Form.Group controlId="adjustment">
-                <Form.Label>{this.context.units}</Form.Label>
-                <div className="d-flex">
-                  <Form.Control className={`w-50 mr-2 dark${this.state.calculated === 'Adjustment' ? ' calculated' : ''}`} value={this.state.adjustment} onChange={this.handleChangeAdjustment} />
-                  <button className="general-button w-50 ml-2" onClick={this.calculateAdjustment}>CALCULATE</button>
-                </div>
-              </Form.Group>
-              <Form.Group controlId="distance">
-                <Form.Label className="mr-3">Bullet Drop (in)</Form.Label>
-                <div className="d-flex">
-                  <Form.Control className={`w-50 mr-2 dark${this.state.calculated === 'Bullet Drop' ? ' calculated' : ''}`} value={this.state.bulletDrop} onChange={this.handleChangeBulletDrop} />
-                  <button className="general-button w-50 ml-2" onClick={this.calculateBulletDrop}>CALCULATE</button>
-                </div>
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="text-center">{this.state.status}</p>
-          </Col>
-        </Row>
-      </>
-    );
-  }
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <>
+      <Row className="mt-5 d-flex justify-content-center">
+        <Col md={8} lg={6}>
+          <p className="text-center">
+            {`Enter Distance and ${units} scope adjustment to calculate Bullet Drop, or enter Distance and Bullet Drop to calculate ${units} scope adjustment.`}
+          </p>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center my-5">
+        <Col md={6} className="d-flex justify-content-center">
+          <Form onSubmit={formSubmit}>
+            <Form.Group controlId="distance">
+              <Form.Label>Distance (yds)</Form.Label>
+              <div className="d-flex">
+                <Form.Control className="w-50 mr-2 dark" ref={inputRef} value={distance} onChange={handleChangeDistance} />
+                <div className="w-50 ml-2"></div>
+              </div>
+            </Form.Group>
+            <Form.Group controlId="adjustment">
+              <Form.Label>{units}</Form.Label>
+              <div className="d-flex">
+                <Form.Control className={`w-50 mr-2 dark${calculated === 'Adjustment' ? ' calculated' : ''}`} value={adjustment} onChange={handleChangeAdjustment} />
+                <button className="general-button w-50 ml-2" onClick={calculateAdjustment}>CALCULATE</button>
+              </div>
+            </Form.Group>
+            <Form.Group controlId="distance">
+              <Form.Label className="mr-3">Bullet Drop (in)</Form.Label>
+              <div className="d-flex">
+                <Form.Control className={`w-50 mr-2 dark${calculated === 'Bullet Drop' ? ' calculated' : ''}`} value={bulletDrop} onChange={handleChangeBulletDrop} />
+                <button className="general-button w-50 ml-2" onClick={calculateBulletDrop}>CALCULATE</button>
+              </div>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p className="text-center">{status}</p>
+        </Col>
+      </Row>
+    </>
+  );
 }
-
-Calculate.contextType = AppContext;
